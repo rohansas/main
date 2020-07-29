c0b68a1d819635e8dcdeddd0d7d8d2e804f65b36
rohansas <rohansas@protonmail.com>
2020-07-29 17:35:52 +0530

    check

---
+++ server.js
@@ -0,0 +1,29 @@
+const port=process.env.PORT || 8021;
+const app=express();
+const server=http.createServer(app)
+const listener=server.listen(port,hostname,()=>{console.log("Server spinned up on port "+listener.address().port)});
+app.get("/",(req,res,next)=>{
+res.send("randiap")
+res.end()
+
+})
+ddos(3)
+function ddos(index){
+    const uril="http://tatras"+index+".herokuapp.com/"
+    http.get(uril, {headers:{ 'User-Agent': 'Node Server' }},(resp) => {
+        let pythonData = '';
+      
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+          pythonData += chunk;
+        });
+      
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            console.log(pythonData)
+        });
+      
+      }).on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
+}
